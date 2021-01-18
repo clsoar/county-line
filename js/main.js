902baf96a8c48dfec248aa94fac501785fa81d79
CL Mason <clsoar@gmail.com>
2021-01-18 18:11:25 -0500

    honeypot added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
 //https://script.google.com/macros/s/AKfycbwRSCpXWL9Id8ihsyBDU5xQQQAVmAW9pZ5G-gy-8iXYsiOXh2hV/exec
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwRSCpXWL9Id8ihsyBDU5xQQQAVmAW9pZ5G-gy-8iXYsiOXh2hV/exec'
-  const form = document.forms['form'];
+const form = document.forms['form'];
+const formSubmitBtn = document.querySelector("#submit-form");
+
+//bad dude protection
+const extra = document.querySelector("#last-name");
+const extra2 = document.querySelector("#name");
+const extra3 = document.querySelector("#password");
 
 
+if(formSubmitBtn) {
+
+  //hide honeypot
+  extra.style.display = "none";
+  extra2.setAttribute('tabindex', "-1");
+
+  if(!extra.value && !extra3.value){
+
+        form.addEventListener('submit', e => {
+            if(!extra.value && extra2.value === "Enter Roboname" && !extra3.value){
+              e.preventDefault();
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => console.log('Success!', response))
+                .catch(error => console.error('Error!', error.message));
+            //clears form
+              form.reset();
+              return false;
+            }
+        })
 
 
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message));
-  //clears form
-    form.reset();
-    return false;
-  })
+    }
+  }
