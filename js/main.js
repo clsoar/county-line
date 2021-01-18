39cf403bb39638bc5d4f01b5fd20af54dda8910e
CL Mason <clsoar@gmail.com>
2021-01-18 14:48:21 -0500

    form reset and selection reqd

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 //https://script.google.com/macros/s/AKfycbwRSCpXWL9Id8ihsyBDU5xQQQAVmAW9pZ5G-gy-8iXYsiOXh2hV/exec
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwRSCpXWL9Id8ihsyBDU5xQQQAVmAW9pZ5G-gy-8iXYsiOXh2hV/exec'
-  const form = document.forms['form']
+  const form = document.forms['form'];
+
+
+
 
   form.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
+      .catch(error => console.error('Error!', error.message));
+  //clears form
+    form.reset();
+    return false;
   })
