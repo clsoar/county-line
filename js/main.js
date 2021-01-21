a65788f80bd843b429d858ffb5f03838b1964fe5
CL Mason <clsoar@gmail.com>
2021-01-21 12:23:01 -0500

    ldg pg for md scrns

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //https://script.google.com/macros/s/AKfycbwRSCpXWL9Id8ihsyBDU5xQQQAVmAW9pZ5G-gy-8iXYsiOXh2hV/exec
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwRSCpXWL9Id8ihsyBDU5xQQQAVmAW9pZ5G-gy-8iXYsiOXh2hV/exec';
-const form = document.forms['form'];
+const form = document.forms;
 const formSubmitBtn = document.querySelector("#submit-form");
 
 //bad dude protection
@@ -18,15 +18,15 @@ if(formSubmitBtn) {
 
   if(!extra.value && !extra3.value){
 
-        form.addEventListener('submit', e => {
-            if(!extra.value && extra2.value === "Enter Roboname" && !extra3.value){
-              e.preventDefault();
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => console.log('Success!', response))
-                .catch(error => console.error('Error!', error.message));
-            //clears form
-              form.reset();
-              return false;
+    form.addEventListener('submit', e => {
+      if(!extra.value && extra2.value === "Enter Roboname" && !extra3.value){
+        e.preventDefault();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then(response => console.log('Success!', response))
+          .catch(error => console.error('Error!', error.message));
+      //clears form
+        form.reset();
+        return false;
             }
         })
 
