1540bc354b5617a2094cccf9dd9376a232c84a92
CL Mason <clsoar@gmail.com>
2021-01-24 15:09:53 -0500

    adding pams js

--- js/main.js
+++ js/main.js
@@ -45,8 +45,51 @@ const transferLocation = () => {
     let thisLong = document.querySelector('#testLong').value;
     newLat.value = thisLat;
     newLong.value = thisLong;
-  
+
   console.log('this might work');
 //  newLat.value = lat;
 //  newLong.value = long;
 };
+//for map
+const yeksOne = "AIzaSyCnWKdGq2iKrYT";
+const yeksTwo = "atd3QqciKceSiyKpU294";
+
+//script element create
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=' + yeksOne + yeksTwo+ '&libraries=geometry,places' + '&callback=initMap';
+script.defer = true;
+//get lat long from Address
+let stAdd = document.querySelector('#st-address').value;
+let city = document.querySelector('#city').value;
+let state = document.querySelector('#state').value;
+let zip = document.querySelector('#zip').value;
+let address = (stAdd + ' ' + city + ', ' + state + ' ' + zip);
+
+//attach callback function to window
+window.initMap = function() {
+  // JS API is loaded and available
+  let geocoder = new google.maps.Geocoder();
+  geocoder.geocode ({
+    'address': address},
+    function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        let latLngs = results[0].geometry.location;
+        let LL = latlngs.toString();
+        let tst = LL.substring(1, LL.length - 1);
+        let coordinates = tst.split(',');
+        var latForMap = coordinates[0];
+        var longForMap = coordinates[1];
+        return (latForMap, longForMap);
+      }
+      return (latForMap, longForMap);
+
+    });
+
+  let map = (latForMap, longForMap) => {new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latForMap, lng: longForMap},
+    zoom: 10
+  });
+};
+}
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
